279e6f0985b55d5afa4167104e4a7a1ff044c9a2
Charlotte Soneson <charlottesoneson@gmail.com>
2022-02-11 16:31:28 +0100

    Define default paths to conv tables/complexes file

---
+++ R/constants.R
@@ -0,0 +1,5 @@
+## Define default paths to files provided via einprot
+
+COMPLEXES_FILE <- system.file("extdata", "complexes", "complexdb_einprot0.5.0_20220211_orthologs.rds")
+WORMBASE_CONVTABLE <- system.file("extdata", "conversion_tables", "WormBaseConv_einprot0.5.0_20220211.rds")
+POMBASE_CONVTABLE <- system.file("extdata", "conversion_tables", "PomBaseConv_einprot0.5.0_20220211.rds")
