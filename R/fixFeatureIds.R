f39735c7eb581547db02178aad8d56a1a0936baa
Charlotte Soneson <charlottesoneson@gmail.com>
2022-02-14 16:14:16 +0100

    Expand documentation

--- R/fixFeatureIds.R
+++ R/fixFeatureIds.R
@@ -1,18 +1,37 @@
+#' Make feature IDs (row names) unique
+#'
+#' Make the feature IDs (row names) of \code{qft} unique by first extracting
+#' the first entry in the \code{geneIdCol} and \code{proteinIdCol} columns
+#' (multiple entries for each row are separated by semicolons), and then
+#' using the gene ID as the feature ID if it exists and is unique, and
+#' otherwise appending the protein ID. If it's still not unique, append an
+#' integer to the name.
+#'
+#' @param qft A \code{QFeatures} object.
+#' @param geneIdCol,proteinIdCol Character scalars indicating which columns of
+#'     \code{rowData(qft[[1]])} should be used as gene/protein identifiers,
+#'     respectively.
+#'
 #' @export
 #' @author Charlotte Soneson
+#'
+#' @return A \code{QFeatures} object with modified, unique row names (see
+#'     description for how these are generated).
 #'
 #' @importFrom SummarizedExperiment rowData
 #'
 fixFeatureIds <- function(qft, geneIdCol = "Gene.names",
                           proteinIdCol = "Majority.protein.IDs") {
     ## Extract the first annotated gene name
-    gName <- vapply(strsplit(rowData(qft[[1]])[[geneIdCol]], ";"),
-                    .subset, 1, FUN.VALUE = "NA")
+    gName <- vapply(strsplit(
+        SummarizedExperiment::rowData(qft[[1]])[[geneIdCol]], ";"),
+        .subset, 1, FUN.VALUE = "NA")
     rowData(qft[[1]])$geneIdSingle <- gName
 
     ## Extract the first annotated majority protein ID
-    majProtID <- vapply(strsplit(rowData(qft[[1]])[[proteinIdCol]], ";"),
-                        .subset, 1, FUN.VALUE = "NA")
+    majProtID <- vapply(strsplit(
+        SummarizedExperiment::rowData(qft[[1]])[[proteinIdCol]], ";"),
+        .subset, 1, FUN.VALUE = "NA")
     rowData(qft[[1]])$proteinIdSingle <- majProtID
 
     ## Find features with missing gene name and replace with the corresponding
