044e92ba7810d4892e3212e9be136f043a003c33
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-07 09:11:34 +0100

    Adapt test setup to use SCE

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,28 +5,39 @@ mqFile <- system.file("extdata", "mq_example", "1356_proteinGroups.txt",
 samples <- c("Adnp_IP04", "Adnp_IP05", "Adnp_IP06",
              "Chd4BF_IP07", "Chd4BF_IP08", "Chd4BF_IP09",
              "RBC_ctrl_IP01", "RBC_ctrl_IP02", "RBC_ctrl_IP03")
-ecol <- paste0("iBAQ.", samples)
-qft_mq_initial <- QFeatures::readQFeatures(mqFile, ecol = ecol, name = "iBAQ",
-                                           sep = "\t", nrows = 70)
+out <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                        includeOnlySamples = samples, nrows = 70)
+sce_mq_initial <- out$sce
+aName <- out$aName
 sampleAnnot <- data.frame(sample = samples,
                           group = gsub("_IP.*", "", samples))
-qft <- addSampleAnnots(qft_mq_initial, iColPattern = "^iBAQ\\.",
-                       sampleAnnot = sampleAnnot, mergeGroups = list())
-qft <- fixFeatureIds(qft)
-qft <- QFeatures::logTransform(qft, base = 2, i = "iBAQ", name = "log2_iBAQ")
-qft <- QFeatures::logTransform(qft, base = 2, i = "iBAQ", name = "log2_iBAQ_withNA")
-tmp <- qft[["log2_iBAQ"]]
-SummarizedExperiment::assay(tmp) <- !is.finite(SummarizedExperiment::assay(tmp))
-qft <- QFeatures::addAssay(qft, tmp, name = "imputed_iBAQ")
-qft <- QFeatures::zeroIsNA(qft, "iBAQ")
-qft <- QFeatures::infIsNA(qft, "log2_iBAQ")
-qft <- QFeatures::infIsNA(qft, "log2_iBAQ_withNA")
-qft_mq_preimputation <- qft
-nbr_na_mq <- QFeatures::nNA(qft_mq_preimputation, i = seq_along(qft_mq_preimputation))
+sce <- addSampleAnnots(sce_mq_initial, sampleAnnot = sampleAnnot, mergeGroups = list())
+sce <- fixFeatureIds(sce, geneIdCol = "Gene.names",
+                     proteinIdCol = "Majority.protein.IDs")
+SummarizedExperiment::assay(sce, paste0("log2_", aName)) <-
+    log2(SummarizedExperiment::assay(sce, aName))
+SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")) <-
+    log2(SummarizedExperiment::assay(sce, aName))
+tmp <- SummarizedExperiment::assay(sce, paste0("log2_", aName))
+tmp <- !is.finite(tmp)
+SummarizedExperiment::assay(sce, paste0("imputed_", aName)) <- tmp
+SummarizedExperiment::assay(sce, aName)[SummarizedExperiment::assay(sce, aName) == 0] <- NA
+SummarizedExperiment::assay(sce, paste0("log2_", aName))[!is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName)))] <- NA
+SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA"))[
+    !is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")))] <- NA
+sce_mq_preimputation <- sce
+nbr_na <- QFeatures::nNA(sce)
+nbr_na <- lapply(nbr_na, function(a) {
+    a$assay <- SummarizedExperiment::assayNames(sce)[1]
+    a
+})
 set.seed(123)
-qft_mq_final <- QFeatures::impute(qft_mq_preimputation, method = "MinProb", i = "log2_iBAQ")
+SummarizedExperiment::assay(sce, paste0("log2_", aName)) <- MsCoreUtils::impute_matrix(
+    SummarizedExperiment::assay(sce, paste0("log2_", aName)), method = "MinProb"
+)
+sce_mq_final <- sce
 fcoll_mq_final <- prepareFeatureCollections(
-    qft = qft_mq_final, idCol = "Gene.names",
+    sce = sce_mq_final, idCol = "Gene.names",
     includeFeatureCollections = "complexes",
     complexDbPath = system.file("extdata", "complexes",
                                 "complexdb_einprot0.5.0_20220211_orthologs.rds",
