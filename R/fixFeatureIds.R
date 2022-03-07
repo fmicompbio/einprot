bc1f0292ff56f4cc22ae679e5dabf03e7864b1a0
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-07 09:19:52 +0100

    Adapt fixFeatureIDs to use SCE

--- R/fixFeatureIds.R
+++ R/fixFeatureIds.R
@@ -1,49 +1,49 @@
 #' Make feature IDs (row names) unique
 #'
-#' Make the feature IDs (row names) of \code{qft} unique by first extracting
+#' Make the feature IDs (row names) of \code{sce} unique by first extracting
 #' the first entry in the \code{geneIdCol} and \code{proteinIdCol} columns
 #' (multiple entries for each row are separated by semicolons), and then
 #' using the gene ID as the feature ID if it exists and is unique, and
 #' otherwise appending the protein ID. If it's still not unique, append an
 #' integer to the name.
 #'
-#' @param qft A \code{QFeatures} object.
+#' @param sce A \code{SummarizedExperiment} object (or derivative).
 #' @param geneIdCol,proteinIdCol Character scalars indicating which columns of
-#'     \code{rowData(qft[[1]])} should be used as gene/protein identifiers,
+#'     \code{rowData(sce)} should be used as gene/protein identifiers,
 #'     respectively.
 #'
 #' @export
 #' @author Charlotte Soneson
 #'
-#' @return A \code{QFeatures} object with modified, unique row names (see
-#'     description for how these are generated).
+#' @return An object of the same type as \code{sce} with modified, unique row
+#'     names (see description for how these are generated).
 #'
 #' @importFrom SummarizedExperiment rowData rowData<-
 #'
-fixFeatureIds <- function(qft, geneIdCol = "Gene.names",
+fixFeatureIds <- function(sce, geneIdCol = "Gene.names",
                           proteinIdCol = "Majority.protein.IDs") {
-    .assertVector(x = qft, type = "QFeatures")
-    vvs <- colnames(SummarizedExperiment::rowData(qft[[1]]))
+    .assertVector(x = sce, type = "SummarizedExperiment")
+    vvs <- colnames(SummarizedExperiment::rowData(sce))
     .assertScalar(x = geneIdCol, type = "character", validValues = vvs)
     .assertScalar(x = proteinIdCol, type = "character", validValues = vvs)
 
     ## Extract the first annotated gene name
     gName <- vapply(strsplit(
-        SummarizedExperiment::rowData(qft[[1]])[[geneIdCol]], ";"),
+        SummarizedExperiment::rowData(sce)[[geneIdCol]], ";"),
         .subset, 1, FUN.VALUE = "NA")
-    rowData(qft[[1]])$geneIdSingle <- gName
+    rowData(sce)$geneIdSingle <- gName
 
     ## Extract the first annotated majority protein ID
     majProtID <- vapply(strsplit(
-        SummarizedExperiment::rowData(qft[[1]])[[proteinIdCol]], ";"),
+        SummarizedExperiment::rowData(sce)[[proteinIdCol]], ";"),
         .subset, 1, FUN.VALUE = "NA")
-    rowData(qft[[1]])$proteinIdSingle <- majProtID
+    rowData(sce)$proteinIdSingle <- majProtID
 
     ## Generate IDs for STRING
     stringIDs <- gName
     idxna <- which(is.na(stringIDs))
     stringIDs[idxna] <- majProtID[idxna]
-    SummarizedExperiment::rowData(qft[[1]])$IDsForSTRING <- stringIDs
+    SummarizedExperiment::rowData(sce)$IDsForSTRING <- stringIDs
 
     ## If there are duplicated gene IDs, make them unique by appending the
     ## respective majority protein ID
@@ -55,7 +55,7 @@ fixFeatureIds <- function(qft, geneIdCol = "Gene.names",
     ## Check that there are no duplicated IDs and set as row names
     gName <- make.unique(gName, sep = ".")
     stopifnot(all(!duplicated(gName)))
-    rownames(qft[[1]]) <- gName
+    rownames(sce) <- gName
 
-    qft
+    sce
 }
--- tests/testthat/test-fixFeatureIds.R
+++ tests/testthat/test-fixFeatureIds.R
@@ -1,79 +1,75 @@
 test_that("fixing feature IDs works", {
     mqFile <- system.file("extdata", "mq_example", "1356_proteinGroups.txt",
                           package = "einprot")
-    samples <- c("Adnp_IP04", "Adnp_IP05", "Adnp_IP06",
-                 "Chd4BF_IP07", "Chd4BF_IP08", "Chd4BF_IP09",
-                 "RBC_ctrl_IP01", "RBC_ctrl_IP02", "RBC_ctrl_IP03")
-    ecol <- paste0("iBAQ.", samples)
-    qft <- QFeatures::readQFeatures(mqFile, ecol = ecol, name = "Intensity",
-                                    sep = "\t", nrows = 25)
+    sce <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                            nrows = 25)$sce
 
     ## Fail with wrong arguments
-    expect_error(fixFeatureIds(qft = 1, geneIdCol = "Gene.names",
+    expect_error(fixFeatureIds(sce = 1, geneIdCol = "Gene.names",
                                proteinIdCol = "Majority.protein.IDs"),
-                 "'qft' must be of class 'QFeatures'")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = 1,
+                 "'sce' must be of class 'SummarizedExperiment'")
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = 1,
                                proteinIdCol = "Majority.protein.IDs"),
                  "'geneIdCol' must be of class 'character'")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = c("Gene.names", "Gene.names"),
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = c("Gene.names", "Gene.names"),
                                proteinIdCol = "Majority.protein.IDs"),
                  "'geneIdCol' must have length 1")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = "missing",
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = "missing",
                                proteinIdCol = "Majority.protein.IDs"),
                  "All values in 'geneIdCol' must be one of")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                                proteinIdCol = 1),
                  "'proteinIdCol' must be of class 'character'")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                                proteinIdCol = c("Majority.protein.IDs",
                                                 "Majority.protein.IDs")),
                  "'proteinIdCol' must have length 1")
-    expect_error(fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    expect_error(fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                                proteinIdCol = "missing"),
                  "All values in 'proteinIdCol' must be one of")
 
     ## Test that it does the right thing
     ## All gene and protein names are unique
-    qft <- QFeatures::readQFeatures(mqFile, ecol = ecol, name = "Intensity",
-                                    sep = "\t", nrows = 25)
-    gns <- SummarizedExperiment::rowData(qft[[1]])$Gene.names
-    pns <- SummarizedExperiment::rowData(qft[[1]])$Majority.protein.IDs
+    sce <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                            nrows = 25)$sce
+    gns <- SummarizedExperiment::rowData(sce)$Gene.names
+    pns <- SummarizedExperiment::rowData(sce)$Majority.protein.IDs
 
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                           proteinIdCol = "Majority.protein.IDs")
-    expect_equal(rownames(qft1[[1]]), vapply(strsplit(gns, ";"),
-                                             .subset, 1, FUN.VALUE = ""))
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Majority.protein.IDs",
+    expect_equal(rownames(sce1), vapply(strsplit(gns, ";"),
+                                        .subset, 1, FUN.VALUE = ""))
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Majority.protein.IDs",
                           proteinIdCol = "Gene.names")
-    expect_equal(rownames(qft1[[1]]), vapply(strsplit(pns, ";"),
-                                             .subset, 1, FUN.VALUE = ""))
+    expect_equal(rownames(sce1), vapply(strsplit(pns, ";"),
+                                        .subset, 1, FUN.VALUE = ""))
 
     ## Missing gene names
-    qft <- QFeatures::readQFeatures(mqFile, ecol = ecol, name = "Intensity",
-                                    sep = "\t", nrows = 45)[36:45, ]
-    gns <- SummarizedExperiment::rowData(qft[[1]])$Gene.names
-    pns <- SummarizedExperiment::rowData(qft[[1]])$Majority.protein.IDs
+    sce <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                            nrows = 45)$sce[36:45, ]
+    gns <- SummarizedExperiment::rowData(sce)$Gene.names
+    pns <- SummarizedExperiment::rowData(sce)$Majority.protein.IDs
 
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                           proteinIdCol = "Majority.protein.IDs")
     midx <- which(gns == "")  ## indices for missing gene IDs
     eidx <- which(gns != "")  ## indices for present gene IDs
-    expect_equal(rownames(qft1[[1]])[eidx], vapply(strsplit(gns[eidx], ";"),
-                                                   .subset, 1, FUN.VALUE = ""))
-    expect_equal(rownames(qft1[[1]])[midx], vapply(strsplit(pns[midx], ";"),
-                                                   .subset, 1, FUN.VALUE = ""))
+    expect_equal(rownames(sce1)[eidx], vapply(strsplit(gns[eidx], ";"),
+                                              .subset, 1, FUN.VALUE = ""))
+    expect_equal(rownames(sce1)[midx], vapply(strsplit(pns[midx], ";"),
+                                              .subset, 1, FUN.VALUE = ""))
     ## Protein names are still there and unique
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Majority.protein.IDs",
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Majority.protein.IDs",
                           proteinIdCol = "Gene.names")
-    expect_equal(rownames(qft1[[1]]), vapply(strsplit(pns, ";"),
-                                             .subset, 1, FUN.VALUE = ""))
+    expect_equal(rownames(sce1), vapply(strsplit(pns, ";"),
+                                        .subset, 1, FUN.VALUE = ""))
 
     ## Duplicated gene names
-    qft <- QFeatures::readQFeatures(mqFile, ecol = ecol, name = "Intensity",
-                                    sep = "\t", nrows = 45)
-    gns <- SummarizedExperiment::rowData(qft[[1]])$Gene.names
-    pns <- SummarizedExperiment::rowData(qft[[1]])$Majority.protein.IDs
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Gene.names",
+    sce <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                            nrows = 45)$sce
+    gns <- SummarizedExperiment::rowData(sce)$Gene.names
+    pns <- SummarizedExperiment::rowData(sce)$Majority.protein.IDs
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Gene.names",
                           proteinIdCol = "Majority.protein.IDs")
     midx <- which(gns == "")  ## indices for missing gene IDs
     gns <- vapply(strsplit(gns, ";"), .subset, 1, FUN.VALUE = "")
@@ -83,11 +79,11 @@ test_that("fixing feature IDs works", {
     expect_length(uidx, 38)
     expect_length(midx, 4)
     expect_length(didx, 3)
-    expect_equal(rownames(qft1[[1]])[uidx], gns[uidx])
-    expect_equal(rownames(qft1[[1]])[midx], pns[midx])
-    expect_equal(rownames(qft1[[1]])[didx], paste0(gns[didx], ".", pns[didx]))
+    expect_equal(rownames(sce1)[uidx], gns[uidx])
+    expect_equal(rownames(sce1)[midx], pns[midx])
+    expect_equal(rownames(sce1)[didx], paste0(gns[didx], ".", pns[didx]))
     ## Protein names are still there and unique
-    qft1 <- fixFeatureIds(qft = qft, geneIdCol = "Majority.protein.IDs",
+    sce1 <- fixFeatureIds(sce = sce, geneIdCol = "Majority.protein.IDs",
                           proteinIdCol = "Gene.names")
-    expect_equal(rownames(qft1[[1]]), pns)
+    expect_equal(rownames(sce1), pns)
 })
