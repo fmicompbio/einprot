d784644893e64a38c55c30f2025d0089accb3525
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-07 16:49:10 +0100

    Modify text snippets

--- R/textSnippets.R
+++ R/textSnippets.R
@@ -9,35 +9,49 @@
 #'
 #' @return A character string.
 #' @examples
-#' limmaText()
-#' ttestText()
+#' testText(testType = "limma")
+#' testText(testType = "ttest")
 NULL
 
 #' @rdname textSnippets
 #' @export
-limmaText <- function() {
-    paste0("For this, we use the treat function from the ",
-           "[limma](https://bioconductor.org/packages/limma/) R/Bioconductor ",
-           "package [@McCarthy2009treat; @Ritchie2015limma; @Phipson2016robust]. ",
-           "For more information about the df.prior, representing the amount of ",
-           "extra information that is borrowed from the full set of features ",
-           "in order to improve the inference for each feature, see section 13.2 in the ",
-           "[limma user guide](https://www.bioconductor.org/packages/devel/bioc/vignettes/limma/inst/doc/usersguide.pdf). ",
-           "In addition to the feature-wise tests, we apply the ",
-           "camera method [@Wu2012camera] to test for significance of each ",
-           "included feature collection. These tests are based on the ",
-           "t-statistic returned from limma.")
+testText <- function(testType) {
+    .assertScalar(x = testType, type = "character",
+                  validValues = c("ttest", "limma"))
+    if (testType == "limma") {
+        paste0("For this, we use the treat function from the ",
+               "[limma](https://bioconductor.org/packages/limma/) R/Bioconductor ",
+               "package [@McCarthy2009treat; @Ritchie2015limma; @Phipson2016robust]. ",
+               "For more information about the df.prior, representing the amount of ",
+               "extra information that is borrowed from the full set of features ",
+               "in order to improve the inference for each feature, see section 13.2 in the ",
+               "[limma user guide](https://www.bioconductor.org/packages/devel/bioc/vignettes/limma/inst/doc/usersguide.pdf). ",
+               "In addition to the feature-wise tests, we apply the ",
+               "camera method [@Wu2012camera] to test for significance of each ",
+               "included feature collection. These tests are based on the ",
+               "t-statistic returned from limma.")
+    } else if (testType == "ttest") {
+        paste0("For this, we use a Student's t-test. To determine which features ",
+               "show significant changes, we calculate the SAM statistic [@Tusher2001sam], ",
+               "and estimate the false discovery rate at different thresholds ",
+               "using permutations, mimicking the approach used by Perseus [@Tyanova2016perseus].",
+               "In addition to the feature-wise tests, we apply the ",
+               "camera method [@Wu2012camera] to test for significance of each ",
+               "included feature collection. These tests are based on the ",
+               "SAM statistic calculated from the t-statistic and the specified S0.")
+    }
 }
 
 #' @rdname textSnippets
 #' @export
-ttestText <- function() {
-    paste0("For this, we use a Student's t-test. To determine which features ",
-           "show significant changes, we calculate the SAM statistic [@Tusher2001sam], ",
-           "and estimate the false discovery rate at different thresholds ",
-           "using permutations, mimicking the approach used by Perseus [@Tyanova2016perseus].",
-           "In addition to the feature-wise tests, we apply the ",
-           "camera method [@Wu2012camera] to test for significance of each ",
-           "included feature collection. These tests are based on the ",
-           "SAM statistic calculated from the t-statistic and the specified S0.")
+normText <- function(normMethod) {
+    .assertScalar(x = normMethod, type = "character")
+
+    if (normMethod == "none") {
+        paste0("The log2 intensities are not normalized further across samples ",
+               "since 'normMethod' is set to 'none'.")
+    } else {
+        paste0("The log2 intensities are next normalized across samples ",
+               "using the ", normMethod, " method.")
+    }
 }
--- tests/testthat/test-textSnippets.R
+++ tests/testthat/test-textSnippets.R
@@ -1,6 +1,25 @@
 test_that("text snippet generation works", {
-    expect_type(limmaText(), "character")
-    expect_equal(length(limmaText()), 1)
-    expect_type(ttestText(), "character")
-    expect_equal(length(ttestText()), 1)
+    ## testText
+    expect_error(testText(testType = 1),
+                 "'testType' must be of class 'character'")
+    expect_error(testText(testType = c("limma", "ttest")),
+                 "'testType' must have length 1")
+    expect_error(testText(testType = "missing"),
+                 "All values in 'testType' must be one of")
+
+    expect_type(testText(testType = "limma"), "character")
+    expect_equal(length(testText(testType = "limma")), 1)
+    expect_type(testText(testType = "limma"), "character")
+    expect_equal(length(testText(testType = "limma")), 1)
+
+    ## normText
+    expect_error(normText(normMethod = 1),
+                 "'normMethod' must be of class 'character'")
+    expect_error(normText(normMethod = c("limma", "ttest")),
+                 "'normMethod' must have length 1")
+
+    expect_type(normText(normMethod = "none"), "character")
+    expect_equal(length(normText(normMethod = "none")), 1)
+    expect_type(normText(normMethod = "center.median"), "character")
+    expect_equal(length(normText(normMethod = "center.median")), 1)
 })
