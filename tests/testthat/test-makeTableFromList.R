1c2bbedc587737037c4cc895adce3f00de3e65e8
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-13 11:17:00 +0100

    More precise tests in makeTableFromList

--- tests/testthat/test-makeTableFromList.R
+++ tests/testthat/test-makeTableFromList.R
@@ -1,8 +1,12 @@
 test_that("makeTableFromList works", {
-    expect_error(makeTableFromList(l = 1))
-    expect_error(makeTableFromList(l = "list"))
-    expect_error(makeTableFromList(l = list(1, 2)))
-    expect_error(makeTableFromList(l = list(x = c(1, 2), y = 3)))
+    expect_error(makeTableFromList(l = 1),
+                 "'l' must be of class 'list'")
+    expect_error(makeTableFromList(l = "list"),
+                 "'l' must be of class 'list'")
+    expect_error(makeTableFromList(l = list(1, 2)),
+                 "'namesl' must not be NULL")
+    expect_error(makeTableFromList(l = list(x = c(1, 2), y = 3)),
+                 "All values in 'vapplyllength0' must be one of")
 
     expect_s3_class(makeTableFromList(list(first = "one",
                                            second = "two", third = 3)),
