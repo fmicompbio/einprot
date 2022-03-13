8df7f5f850beac56322ce8d4d59b2a944d3a5fda
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-13 20:09:34 +0100

    Add test to plotImputation

--- tests/testthat/test-plotImputation.R
+++ tests/testthat/test-plotImputation.R
@@ -42,11 +42,26 @@ test_that("plotting of imputation results works", {
         xlab = c("lab1", "lab2")),
         "'xlab' must have length 1")
 
-    out <- plotImputationDistribution(sce = sce_mq_final, assayToPlot = "log2_iBAQ",
-                                      assayImputation = "imputed_iBAQ",
-                                      xlab = "lab")
+    out <- plotImputationDistribution(
+        sce = sce_mq_final, assayToPlot = "log2_iBAQ",
+        assayImputation = "imputed_iBAQ", xlab = "lab")
     expect_s3_class(out, "ggplot")
     expect_true(any(out$data$imputed))
     expect_true(any(!out$data$imputed))
     expect_false(any(grepl("^iBAQ\\.", out$data$sample)))
+    expect_equal(nrow(out$data), 70 * 9)
+    expect_equal(ncol(out$data), 4)
+    expect_named(out$data, c("pid", "sample", "log2intensity", "imputed"))
+
+    ## PD input
+    out <- plotImputationDistribution(
+        sce = sce_pd_final, assayToPlot = "log2_Abundance",
+        assayImputation = "imputed_Abundance", xlab = "lab")
+    expect_s3_class(out, "ggplot")
+    expect_true(any(out$data$imputed))
+    expect_true(any(!out$data$imputed))
+    expect_false(any(grepl("^Abundance\\.", out$data$sample)))
+    expect_equal(nrow(out$data), 70 * 16)
+    expect_equal(ncol(out$data), 4)
+    expect_named(out$data, c("pid", "sample", "log2intensity", "imputed"))
 })
