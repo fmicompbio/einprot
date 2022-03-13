d90a8cae0b4e0dca9c9a83d0292819d1fa85ab99
Charlotte Soneson <charlottesoneson@gmail.com>
2022-03-13 10:50:24 +0100

    Clarify setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,43 +1,45 @@
-## Preparation of MQ SummarizedExperiment object
+## Preparation of MQ SCE object
 ## ------------------------------------------------------------------------- ##
 mqFile <- system.file("extdata", "mq_example", "1356_proteinGroups.txt",
                       package = "einprot")
-samples <- c("Adnp_IP04", "Adnp_IP05", "Adnp_IP06",
-             "Chd4BF_IP07", "Chd4BF_IP08", "Chd4BF_IP09",
-             "RBC_ctrl_IP01", "RBC_ctrl_IP02", "RBC_ctrl_IP03")
-out <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
-                        includeOnlySamples = samples, nrows = 70)
-sce_mq_initial <- out$sce
-aName <- out$aName
-sampleAnnot <- data.frame(sample = samples,
-                          group = gsub("_IP.*", "", samples))
-sce <- addSampleAnnots(sce_mq_initial, sampleAnnot = sampleAnnot, mergeGroups = list())
-sce <- fixFeatureIds(sce, geneIdCol = "Gene.names",
-                     proteinIdCol = "Majority.protein.IDs")
-SummarizedExperiment::assay(sce, paste0("log2_", aName)) <-
-    log2(SummarizedExperiment::assay(sce, aName))
-SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")) <-
-    log2(SummarizedExperiment::assay(sce, aName))
-tmp <- SummarizedExperiment::assay(sce, paste0("log2_", aName))
+mqSamples <- c("Adnp_IP04", "Adnp_IP05", "Adnp_IP06",
+               "Chd4BF_IP07", "Chd4BF_IP08", "Chd4BF_IP09",
+               "RBC_ctrl_IP01", "RBC_ctrl_IP02", "RBC_ctrl_IP03")
+mqOut <- importExperiment(inFile = mqFile, iColPattern = "^iBAQ\\.",
+                          includeOnlySamples = mqSamples, nrows = 70)
+mqsce <- mqOut$sce
+sce_mq_initial <- mqsce   ## initial object after import
+mqaName <- mqOut$aName
+mqSampleAnnot <- data.frame(sample = mqSamples,
+                            group = gsub("_IP.*", "", mqSamples))
+mqsce <- addSampleAnnots(mqsce, sampleAnnot = mqSampleAnnot,
+                         mergeGroups = list())
+mqsce <- fixFeatureIds(mqsce, geneIdCol = "Gene.names",
+                       proteinIdCol = "Majority.protein.IDs")
+SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName)) <-
+    log2(SummarizedExperiment::assay(mqsce, mqaName))
+SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName, "_withNA")) <-
+    log2(SummarizedExperiment::assay(mqsce, mqaName))
+tmp <- SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName))
 tmp <- !is.finite(tmp)
-SummarizedExperiment::assay(sce, paste0("imputed_", aName)) <- tmp
-SummarizedExperiment::assay(sce, aName)[SummarizedExperiment::assay(sce, aName) == 0] <- NA
-SummarizedExperiment::assay(sce, paste0("log2_", aName))[!is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName)))] <- NA
-SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA"))[
-    !is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")))] <- NA
-sce_mq_preimputation <- sce
-nbr_na_mq <- QFeatures::nNA(sce)
+SummarizedExperiment::assay(mqsce, paste0("imputed_", mqaName)) <- tmp
+SummarizedExperiment::assay(mqsce, mqaName)[SummarizedExperiment::assay(mqsce, mqaName) == 0] <- NA
+SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName))[!is.finite(SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName)))] <- NA
+SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName, "_withNA"))[
+    !is.finite(SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName, "_withNA")))] <- NA
+sce_mq_preimputation <- mqsce   ## fixed features, NAs for missing values
+nbr_na_mq <- QFeatures::nNA(mqsce)
 nbr_na_mq <- lapply(nbr_na_mq, function(a) {
-    a$assay <- SummarizedExperiment::assayNames(sce)[1]
+    a$assay <- SummarizedExperiment::assayNames(mqsce)[1]
     a
 })
 set.seed(123)
-SummarizedExperiment::assay(sce, paste0("log2_", aName)) <- MsCoreUtils::impute_matrix(
-    SummarizedExperiment::assay(sce, paste0("log2_", aName)), method = "MinProb"
+SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName)) <- MsCoreUtils::impute_matrix(
+    SummarizedExperiment::assay(mqsce, paste0("log2_", mqaName)), method = "MinProb"
 )
-sce_mq_final <- sce
+sce_mq_final <- mqsce   ## final object
 fcoll_mq_final <- prepareFeatureCollections(
-    sce = sce_mq_final, idCol = "Gene.names",
+    sce = mqsce, idCol = "Gene.names",
     includeFeatureCollections = "complexes",
     complexDbPath = system.file("extdata", "complexes",
                                 "complexdb_einprot0.5.0_20220211_orthologs.rds",
@@ -45,53 +47,54 @@ fcoll_mq_final <- prepareFeatureCollections(
     speciesInfo = getSpeciesInfo("mouse"), complexSpecies = "current",
     customComplexes = list(), minSizeToKeep = 2)
 
-## Preparation of PD TMT SummarizedExperiment object
+## Preparation of PD TMT SCE object
 ## ------------------------------------------------------------------------- ##
 pdOutputFolder <- system.file("extdata", "pdtmt_example", package = "einprot")
 pdResultName <- "Fig2_m23139_RTS_QC_varMods"
 pdAnalysisFile <- system.file("extdata", "pdtmt_example",
                               "Fig2_m23139_RTS_QC_varMods.pdAnalysis",
                               package = "einprot")
-iColPattern = "^Abundance\\.F.+\\.Sample\\."
-sampleAnnot = data.frame(
+pdSampleAnnot = data.frame(
     sample = c("HIS4KO_S05", "HIS4KO_S06", "HIS4KO_S07", "HIS4KO_S08",
                "MET6KO_S01", "MET6KO_S02", "MET6KO_S03", "MET6KO_S04",
                "URA2KO_S09", "URA2KO_S10", "URA2KO_S11", "URA2KO_S12",
                "WT_S13", "WT_S14", "WT_S15", "WT_S16"),
     group = c(rep("HIS4KO", 4), rep("MET6KO", 4), rep("URA2KO", 4),
               rep("WT", 4)))
-out <- importExperiment(
+pdOut <- importExperiment(
     inFile = file.path(pdOutputFolder, paste0(pdResultName, "_Proteins.txt")),
-    iColPattern = iColPattern, nrows = 70)
-sce_pd_initial <- out$sce
-aName <- out$aName
-sce <- addSampleAnnots(sce_pd_initial, sampleAnnot = sampleAnnot, mergeGroups = list())
-sce <- fixFeatureIds(sce, geneIdCol = "Gene.Symbol",
-                     proteinIdCol = "Accession")
-SummarizedExperiment::assay(sce, paste0("log2_", aName)) <-
-    log2(SummarizedExperiment::assay(sce, aName))
-SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")) <-
-    log2(SummarizedExperiment::assay(sce, aName))
-tmp <- SummarizedExperiment::assay(sce, paste0("log2_", aName))
+    iColPattern = "^Abundance\\.F.+\\.Sample\\.", nrows = 70)
+pdsce <- pdOut$sce
+sce_pd_initial <- pdsce
+pdaName <- pdOut$aName
+pdsce <- addSampleAnnots(pdsce, sampleAnnot = pdSampleAnnot,
+                         mergeGroups = list())
+pdsce <- fixFeatureIds(pdsce, geneIdCol = "Gene.Symbol",
+                       proteinIdCol = "Accession")
+SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName)) <-
+    log2(SummarizedExperiment::assay(pdsce, pdaName))
+SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName, "_withNA")) <-
+    log2(SummarizedExperiment::assay(pdsce, pdaName))
+tmp <- SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName))
 tmp <- !is.finite(tmp)
-SummarizedExperiment::assay(sce, paste0("imputed_", aName)) <- tmp
-SummarizedExperiment::assay(sce, aName)[SummarizedExperiment::assay(sce, aName) == 0] <- NA
-SummarizedExperiment::assay(sce, paste0("log2_", aName))[!is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName)))] <- NA
-SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA"))[
-    !is.finite(SummarizedExperiment::assay(sce, paste0("log2_", aName, "_withNA")))] <- NA
-sce_pd_preimputation <- sce
-nbr_na_pd <- QFeatures::nNA(sce)
+SummarizedExperiment::assay(pdsce, paste0("imputed_", pdaName)) <- tmp
+SummarizedExperiment::assay(pdsce, pdaName)[SummarizedExperiment::assay(pdsce, pdaName) == 0] <- NA
+SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName))[!is.finite(SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName)))] <- NA
+SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName, "_withNA"))[
+    !is.finite(SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName, "_withNA")))] <- NA
+sce_pd_preimputation <- pdsce
+nbr_na_pd <- QFeatures::nNA(pdsce)
 nbr_na_pd <- lapply(nbr_na_pd, function(a) {
-    a$assay <- SummarizedExperiment::assayNames(sce)[1]
+    a$assay <- SummarizedExperiment::assayNames(pdsce)[1]
     a
 })
 set.seed(123)
-SummarizedExperiment::assay(sce, paste0("log2_", aName)) <- MsCoreUtils::impute_matrix(
-    SummarizedExperiment::assay(sce, paste0("log2_", aName)), method = "MinProb"
+SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName)) <- MsCoreUtils::impute_matrix(
+    SummarizedExperiment::assay(pdsce, paste0("log2_", pdaName)), method = "MinProb"
 )
-sce_pd_final <- sce
+sce_pd_final <- pdsce
 fcoll_pd_final <- prepareFeatureCollections(
-    sce = sce_pd_final, idCol = "Gene.Symbol",
+    sce = pdsce, idCol = "Gene.Symbol",
     includeFeatureCollections = "complexes",
     complexDbPath = system.file("extdata", "complexes",
                                 "complexdb_einprot0.5.0_20220211_orthologs.rds",
--- tests/testthat/test-addSampleAnnots.R
+++ tests/testthat/test-addSampleAnnots.R
@@ -3,7 +3,7 @@ test_that("adding sample annotations works", {
     ## --------------------------------------------------------------------- ##
     ## Fail with wrong arguments
     expect_error(addSampleAnnots(1,
-                                 sampleAnnot = sampleAnnot,
+                                 sampleAnnot = mqSampleAnnot,
                                  mergeGroups = list()),
                  "'sce' must be of class 'SummarizedExperiment'")
     expect_error(addSampleAnnots(sce_mq_initial,
@@ -11,52 +11,52 @@ test_that("adding sample annotations works", {
                                  mergeGroups = list()),
                  "'sampleAnnot' must be of class 'data.frame'")
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = as.matrix(sampleAnnot),
+                                 sampleAnnot = as.matrix(mqSampleAnnot),
                                  mergeGroups = list()),
                  "'sampleAnnot' must be of class 'data.frame'")
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = S4Vectors::DataFrame(sampleAnnot),
+                                 sampleAnnot = S4Vectors::DataFrame(mqSampleAnnot),
                                  mergeGroups = list()),
                  "'sampleAnnot' must be of class 'data.frame'")
-    sampleAnnot1 <- sampleAnnot
+    sampleAnnot1 <- mqSampleAnnot
     colnames(sampleAnnot1) <- c("sample", "group1")
     expect_error(addSampleAnnots(sce_mq_initial,
                                  sampleAnnot = sampleAnnot1,
                                  mergeGroups = list()),
                  "colnames(sampleAnnot)) is not TRUE", fixed = TRUE)
-    sampleAnnot1 <- sampleAnnot
+    sampleAnnot1 <- mqSampleAnnot
     colnames(sampleAnnot1) <- c("sample1", "group")
     expect_error(addSampleAnnots(sce_mq_initial,
                                  sampleAnnot = sampleAnnot1,
                                  mergeGroups = list()),
                  "colnames(sampleAnnot)) is not TRUE", fixed = TRUE)
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = rbind(sampleAnnot, sampleAnnot),
+                                 sampleAnnot = rbind(mqSampleAnnot, mqSampleAnnot),
                                  mergeGroups = list()),
                  "all(!duplicated(sampleAnnot$sample)) is not TRUE", fixed = TRUE)
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = sampleAnnot,
+                                 sampleAnnot = mqSampleAnnot,
                                  mergeGroups = c("Adnp", "RBC_ctrl")),
                  "'mergeGroups' must be of class 'list'")
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = sampleAnnot,
+                                 sampleAnnot = mqSampleAnnot,
                                  mergeGroups = list(c("Adnp", "RBC_ctrl"))),
                  "'namesmergeGroups' must not be NULL")
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = sampleAnnot,
+                                 sampleAnnot = mqSampleAnnot,
                                  mergeGroups = list(g1 = "Adnp",
                                                     g1 = "RBC_ctrl")),
                  "'mergeGroups' must be a named list, without duplicated")
     expect_error(addSampleAnnots(sce_mq_initial,
-                                 sampleAnnot = sampleAnnot,
+                                 sampleAnnot = mqSampleAnnot,
                                  mergeGroups = list(g1 = "Adnp",
                                                     g12 = c("Adnp", "RBC_ctrl"))),
                  "A given name can just be part of one merged group")
 
     ## --------------------------------------------------------------------- ##
     ## Add sample annotations
-    sampleAnnot <- data.frame(sample = samples,
-                              group = gsub("_IP.*", "", samples))
+    sampleAnnot <- data.frame(sample = mqSamples,
+                              group = gsub("_IP.*", "", mqSamples))
     sce1 <- addSampleAnnots(sce_mq_initial,
                             sampleAnnot = sampleAnnot, mergeGroups = list())
     cdt1 <- SummarizedExperiment::colData(sce1)
@@ -68,8 +68,8 @@ test_that("adding sample annotations works", {
     expect_equal(cdt1$group, sampleAnnot$group)
 
     ## Provide sample annotations in different order
-    sampleAnnot <- data.frame(sample = samples,
-                              group = gsub("_IP.*", "", samples))
+    sampleAnnot <- data.frame(sample = mqSamples,
+                              group = gsub("_IP.*", "", mqSamples))
     set.seed(123)
     sampleAnnot <- sampleAnnot[sample(seq_len(nrow(sampleAnnot)),
                                       nrow(sampleAnnot)), ]
@@ -85,8 +85,8 @@ test_that("adding sample annotations works", {
     expect_equal(cdt1$group, sampleAnnot$group[ordr])
 
     ## Extra samples in annotation
-    sampleAnnot <- data.frame(sample = c(samples, paste0(samples, "_rep2")),
-                              group = rep(gsub("_IP.*", "", samples), 2))
+    sampleAnnot <- data.frame(sample = c(mqSamples, paste0(mqSamples, "_rep2")),
+                              group = rep(gsub("_IP.*", "", mqSamples), 2))
     sce1 <- addSampleAnnots(sce_mq_initial,
                             sampleAnnot = sampleAnnot, mergeGroups = list())
     cdt1 <- SummarizedExperiment::colData(sce1)
@@ -94,15 +94,15 @@ test_that("adding sample annotations works", {
     expect_s4_class(cdt1, "DFrame")
     expect_named(cdt1, c("sample", "group_orig", "group"))
     expect_equal(cdt1$group_orig, cdt1$group)
-    expect_equal(cdt1$sample, samples)
+    expect_equal(cdt1$sample, mqSamples)
     expect_equal(cdt1$group, sampleAnnot$group[seq_len(nrow(cdt1))])
 
     ## Additional columns in sampleAnnot - only batch will be used
     set.seed(123)
-    sampleAnnot <- data.frame(sample = samples,
-                              group = gsub("_IP.*", "", samples),
+    sampleAnnot <- data.frame(sample = mqSamples,
+                              group = gsub("_IP.*", "", mqSamples),
                               batch = rep(c("b1", "b2"), c(4, 5)),
-                              age = 100 * runif(length(samples)))
+                              age = 100 * runif(length(mqSamples)))
     sce1 <- addSampleAnnots(sce_mq_initial,
                             sampleAnnot = sampleAnnot, mergeGroups = list())
     cdt1 <- SummarizedExperiment::colData(sce1)
@@ -114,32 +114,32 @@ test_that("adding sample annotations works", {
     expect_equal(cdt1$group, sampleAnnot$group)
 
     ## Missing samples in annotation - should fail
-    sampleAnnot <- data.frame(sample = samples[1:7],
-                              group = gsub("_IP.*", "", samples[1:7]))
+    sampleAnnot <- data.frame(sample = mqSamples[1:7],
+                              group = gsub("_IP.*", "", mqSamples[1:7]))
     expect_error(addSampleAnnots(sce_mq_initial,
                                  sampleAnnot = sampleAnnot, mergeGroups = list()),
                  "Some samples are missing")
 
     ## Include iColPattern in sample names - should fail
-    sampleAnnot <- data.frame(sample = paste0("iBAQ.", samples),
-                              group = gsub("_IP.*", "", samples))
+    sampleAnnot <- data.frame(sample = paste0("iBAQ.", mqSamples),
+                              group = gsub("_IP.*", "", mqSamples))
     expect_error(addSampleAnnots(sce_mq_initial,
                                  sampleAnnot = sampleAnnot, mergeGroups = list()),
                  "Some samples are missing")
 
     ## Merge groups
-    sampleAnnot <- data.frame(sample = samples,
-                              group = gsub("_IP.*", "", samples))
+    sampleAnnot <- data.frame(sample = mqSamples,
+                              group = gsub("_IP.*", "", mqSamples))
     sce1 <- addSampleAnnots(sce_mq_initial,
-                            sampleAnnot = sampleAnnot,
+                            sampleAnnot = mqSampleAnnot,
                             mergeGroups = list(G1 = c("Adnp", "RBC_ctrl"),
                                                G2 = "Chd4BF"))
     cdt1 <- SummarizedExperiment::colData(sce1)
     expect_s4_class(sce1, "SummarizedExperiment")
     expect_s4_class(cdt1, "DFrame")
     expect_named(cdt1, c("sample", "group_orig", "group"))
-    expect_equal(cdt1$sample, sampleAnnot$sample)
-    expect_equal(cdt1$group_orig, sampleAnnot$group)
+    expect_equal(cdt1$sample, mqSampleAnnot$sample)
+    expect_equal(cdt1$group_orig, mqSampleAnnot$group)
     expect_true(all(cdt1$group[cdt1$group_orig %in% c("Adnp", "RBC_ctrl")] == "G1"))
     expect_true(all(cdt1$group[cdt1$group_orig %in% c("Chd4BF")] == "G2"))
 })
