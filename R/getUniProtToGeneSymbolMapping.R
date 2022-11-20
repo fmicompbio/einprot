78dc2bc42714776bf4b3eb55b848926c21ed891b
Charlotte Soneson <charlottesoneson@gmail.com>
2022-11-20 16:13:15 +0100

    Add tests for uniprot to symbol mapping

--- R/getUniProtToGeneSymbolMapping.R
+++ R/getUniProtToGeneSymbolMapping.R
@@ -1,3 +1,30 @@
+.getUniProtToGeneSymbolMappingFile <- function(spi) {
+    .assertVector(x = spi, type = "list")
+    stopifnot("species" %in% names(spi))
+
+    ## Get file name
+    if (spi$species == "Mus musculus") {
+        f <- "MOUSE_10090_idmapping.dat.gz"
+    } else if (spi$species == "Homo sapiens") {
+        f <- "HUMAN_9606_idmapping.dat.gz"
+    } else if (spi$species == "Caenorhabditis elegans") {
+        f <- "CAEEL_6239_idmapping.dat.gz"
+    } else if (spi$species == "Danio rerio") {
+        f <- "DANRE_7955_idmapping.dat.gz"
+    } else if (spi$species == "Drosophila melanogaster") {
+        f <- "DROME_7227_idmapping.dat.gz"
+    } else if (spi$species == "Saccharomyces cerevisiae") {
+        ## taxId 4932 is not available
+        f <- "YEAST_559292_idmapping.dat.gz"
+    } else if (spi$species == "Schizosaccharomyces pombe 972h-") {
+        f <- "SCHPO_284812_idmapping.dat.gz"
+    } else {
+        stop("Unsupported species")
+    }
+
+    f
+}
+
 #' Get mapping from UniProt IDs to gene symbol
 #'
 #' Generate a data.frame with the mapping between UniProtIDs and gene symbols.
@@ -22,25 +49,7 @@
 getUniProtToGeneSymbolMapping <- function(species) {
     spi <- getSpeciesInfo(species)
 
-    ## Get file name
-    if (spi$species == "Mus musculus") {
-        f <- "MOUSE_10090_idmapping.dat.gz"
-    } else if (spi$species == "Homo sapiens") {
-        f <- "HUMAN_9606_idmapping.dat.gz"
-    } else if (spi$species == "Caenorhabditis elegans") {
-        f <- "CAEEL_6239_idmapping.dat.gz"
-    } else if (spi$species == "Danio rerio") {
-        f <- "DANRE_7955_idmapping.dat.gz"
-    } else if (spi$species == "Drosophila melanogaster") {
-        f <- "DROME_7227_idmapping.dat.gz"
-    } else if (spi$species == "Saccharomyces cerevisiae") {
-        ## taxId 4932 is not available
-        f <- "YEAST_559292_idmapping.dat.gz"
-    } else if (spi$species == "Schizosaccharomyces pombe 972h-") {
-        f <- "SCHPO_284812_idmapping.dat.gz"
-    } else {
-        stop("Unsupported species")
-    }
+    f <- .getUniProtToGeneSymbolMappingFile(spi)
 
     readr::read_tsv(gzcon(
         url(paste0("https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/", f))),
--- tests/testthat/test-getUniProtToGeneSymbolMapping.R
+++ tests/testthat/test-getUniProtToGeneSymbolMapping.R
@@ -2,6 +2,27 @@ test_that("getUniProtToGeneSymbolMapping works", {
     expect_error(getUniProtToGeneSymbolMapping(1), "Unknown species")
     expect_error(getUniProtToGeneSymbolMapping("wrongSpecies"), "Unknown species")
 
+    expect_error(.getUniProtToGeneSymbolMappingFile("mouse"),
+                 "'spi' must be of class 'list'")
+    expect_error(.getUniProtToGeneSymbolMappingFile(list(a = "mouse")),
+                 "%in% names(spi) is not TRUE", fixed = TRUE)
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("mouse")),
+                 "MOUSE_10090_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("human")),
+                 "HUMAN_9606_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("roundworm")),
+                 "CAEEL_6239_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("zebrafish")),
+                 "DANRE_7955_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("fruitfly")),
+                 "DROME_7227_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("baker's yeast")),
+                 "YEAST_559292_idmapping.dat.gz")
+    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("fission yeast")),
+                 "SCHPO_284812_idmapping.dat.gz")
+    expect_error(.getUniProtToGeneSymbolMappingFile(list(species = "missing")),
+                 "Unsupported species")
+
     df <- getUniProtToGeneSymbolMapping("fruitfly")
     expect_s3_class(df, "data.frame")
     expect_equal(ncol(df), 2)
