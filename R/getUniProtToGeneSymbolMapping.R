c5975d43ecf5fc44267077b6d48f34cd4f72c5a1
Charlotte Soneson <charlottesoneson@gmail.com>
2023-01-03 15:42:06 +0100

    Allow mapping UniProtIDs to any target ID type

--- R/getUniProtToGeneSymbolMapping.R
+++ R/getUniProtToGeneSymbolMapping.R
@@ -1,4 +1,4 @@
-.getUniProtToGeneSymbolMappingFile <- function(spi) {
+.getUniProtToIDMappingFile <- function(spi) {
     .assertVector(x = spi, type = "list")
     stopifnot("species" %in% names(spi))
 
@@ -25,39 +25,46 @@
     f
 }
 
-#' Get mapping from UniProt IDs to gene symbol
+#' Get mapping from UniProt IDs to another ID type
 #'
-#' Generate a data.frame with the mapping between UniProtIDs and gene symbols.
+#' Generate a data.frame with the mapping between UniProtIDs and another ID type.
 #' The mapping is obtained from the UniProt ID mapping files (downloaded from
 #' https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/).
 #'
 #' @param species Either a taxonomy ID, a species ID or a common species name.
 #'     See \code{getSupportedSpecies()} for valid values.
+#' @param targetId Character scalar giving the target ID type. Should be one
+#'     of the ID types included in the second column in the UniProt ID mapping
+#'     files. Examples include "Gene_Name", "Ensembl", "STRING", "RefSeq_NT"
+#'     (depending on the species).
 #'
 #' @export
 #' @author Charlotte Soneson
 #'
 #' @return A data.frame with two columns, corresponding to the UniProtID and
-#'     the corresponding gene symbol. Note that both columns can have
+#'     the corresponding other ID. Note that both columns can have
 #'     duplicated values, if there is not a one-to-one mapping between
-#'     UniProtIDs and gene symbols.
+#'     UniProtIDs and the other ID type.
 #'
 #' @importFrom readr read_tsv
 #' @importFrom tidyr pivot_wider unnest
 #' @importFrom dplyr filter rename
 #'
-getUniProtToGeneSymbolMapping <- function(species) {
+getUniProtToIDMapping <- function(species, targetId = "Gene_Name") {
+    .assertScalar(x = targetId, type = "character")
+
     spi <- getSpeciesInfo(species)
 
-    f <- .getUniProtToGeneSymbolMappingFile(spi)
+    f <- .getUniProtToIDMappingFile(spi)
 
     readr::read_tsv(gzcon(
-        url(paste0("https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/", f))),
+        url(paste0("https://ftp.uniprot.org/pub/databases/uniprot/",
+                   "current_release/knowledgebase/idmapping/by_organism/", f))),
         col_names = FALSE, col_types = "ccc") %>%
-        dplyr::filter(.data$X2 %in% c("Gene_Name")) %>%
+        dplyr::filter(.data$X2 %in% targetId) %>%
         tidyr::pivot_wider(id_cols = "X1", names_from = "X2",
                            values_from = "X3", values_fn = list) %>%
-        tidyr::unnest(cols = c("Gene_Name")) %>%
-        dplyr::rename(UniProtID = "X1", GeneSymbol = "Gene_Name") %>%
+        tidyr::unnest(cols = .data[[targetId]]) %>%
+        dplyr::rename(UniProtID = "X1") %>%
         as.data.frame()
 }
--- tests/testthat/test-getUniProtToGeneSymbolMapping.R
+++ tests/testthat/test-getUniProtToGeneSymbolMapping.R
@@ -1,30 +1,37 @@
-test_that("getUniProtToGeneSymbolMapping works", {
-    expect_error(getUniProtToGeneSymbolMapping(1), "Unknown species")
-    expect_error(getUniProtToGeneSymbolMapping("wrongSpecies"), "Unknown species")
+test_that("getUniProtToIDMapping works", {
+    expect_error(getUniProtToIDMapping(1), "Unknown species")
+    expect_error(getUniProtToIDMapping("wrongSpecies"), "Unknown species")
+    expect_error(getUniProtToIDMapping("fruitfly", targetId = 1),
+                 "'targetId' must be of class 'character'")
 
-    expect_error(.getUniProtToGeneSymbolMappingFile("mouse"),
+    expect_error(.getUniProtToIDMappingFile("mouse"),
                  "'spi' must be of class 'list'")
-    expect_error(.getUniProtToGeneSymbolMappingFile(list(a = "mouse")),
+    expect_error(.getUniProtToIDMappingFile(list(a = "mouse")),
                  "%in% names(spi) is not TRUE", fixed = TRUE)
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("mouse")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("mouse")),
                  "MOUSE_10090_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("human")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("human")),
                  "HUMAN_9606_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("roundworm")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("roundworm")),
                  "CAEEL_6239_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("zebrafish")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("zebrafish")),
                  "DANRE_7955_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("fruitfly")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("fruitfly")),
                  "DROME_7227_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("baker's yeast")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("baker's yeast")),
                  "YEAST_559292_idmapping.dat.gz")
-    expect_equal(.getUniProtToGeneSymbolMappingFile(getSpeciesInfo("fission yeast")),
+    expect_equal(.getUniProtToIDMappingFile(getSpeciesInfo("fission yeast")),
                  "SCHPO_284812_idmapping.dat.gz")
-    expect_error(.getUniProtToGeneSymbolMappingFile(list(species = "missing")),
+    expect_error(.getUniProtToIDMappingFile(list(species = "missing")),
                  "Unsupported species")
 
-    df <- getUniProtToGeneSymbolMapping("fruitfly")
+    df <- getUniProtToIDMapping("fruitfly")
     expect_s3_class(df, "data.frame")
     expect_equal(ncol(df), 2)
-    expect_named(df, c("UniProtID", "GeneSymbol"))
+    expect_named(df, c("UniProtID", "Gene_Name"))
+
+    df <- getUniProtToIDMapping("fruitfly", targetId = "RefSeq_NT")
+    expect_s3_class(df, "data.frame")
+    expect_equal(ncol(df), 2)
+    expect_named(df, c("UniProtID", "RefSeq_NT"))
 })
