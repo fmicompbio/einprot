e34658e848914928db80f0e2bc13712c718f7556
Charlotte Soneson <charlottesoneson@gmail.com>
2023-07-01 14:52:40 +0200

    Allow multiple abundance prefixes for makeAbundanceExport

--- tests/testthat/test-makeAbundanceExport.R
+++ tests/testthat/test-makeAbundanceExport.R
@@ -3,10 +3,9 @@ test_that("makeAbundanceExport works", {
                  "'testresList' must be of class 'list'")
     expect_error(makeAbundanceExport(testresList = list(), abundancePrefix = 1),
                  "'abundancePrefix' must be of class 'character'")
-    expect_error(makeAbundanceExport(testresList = list(),
-                                     abundancePrefix = c("pref", "pref2")),
-                 "'abundancePrefix' must have length 1")
 
+    ## Single aName
+    ## -------------------------------------------------------------------------
     out_limma <- runTest(
         sce = sce_mq_final,
         comparisons = list(c("Adnp", "RBC_ctrl"), c("RBC_ctrl", "Chd4BF")),
@@ -57,6 +56,75 @@ test_that("makeAbundanceExport works", {
                  ignore_attr = TRUE)
     expect_equal(abExp$iBAQ.Adnp.sd,
                  rowSds(abExp[, c(grep("iBAQ.Adnp_", colnames(abExp)))],
+                        na.rm = TRUE),
+                 ignore_attr = TRUE)
+
+    ## Multiple aNames
+    ## -------------------------------------------------------------------------
+    out_limma <- runTest(
+        sce = sce_mq_final,
+        comparisons = list(c("Adnp", "RBC_ctrl"), c("RBC_ctrl", "Chd4BF")),
+        testType = "limma",
+        assayForTests = "log2_iBAQ", assayImputation = "imputed_iBAQ",
+        minNbrValidValues = 2, minlFC = 0, featureCollections = fcoll_mq_final,
+        complexFDRThr = 0.8, volcanoAdjPvalThr = 0.05, volcanoLog2FCThr = 1,
+        baseFileName = NULL, seed = 123, nperm = 25, volcanoS0 = 0.1,
+        addAbundanceValues = TRUE, aName = c("iBAQ", "LFQ.intensity"),
+        singleFit = FALSE
+    )
+    abExp <- makeAbundanceExport(testresList = out_limma$tests,
+                                 abundancePrefix = c("iBAQ", "LFQ.intensity"))
+    expect_s3_class(abExp, "data.frame")
+    expect_equal(nrow(abExp), sum(out_limma$tests$RBC_ctrl_vs_Adnp$showInVolcano |
+                                      out_limma$tests$Chd4BF_vs_RBC_ctrl$showInVolcano,
+                                  na.rm = TRUE))
+    expect_equal(ncol(abExp), 33L)
+    expect_equal(abExp[, paste0("iBAQ.", colnames(sce_mq_final))],
+                 as.data.frame(SummarizedExperiment::assay(sce_mq_final, "iBAQ")[abExp$pid, ]),
+                 ignore_attr = TRUE)
+    expect_equal(abExp[, paste0("LFQ.intensity.", colnames(sce_mq_final))],
+                 as.data.frame(SummarizedExperiment::assay(sce_mq_final,
+                                                           "LFQ.intensity")[abExp$pid, ]),
+                 ignore_attr = TRUE)
+    expect_equal(sort(abExp$pid[abExp$showInVolcano_Chd4BF_vs_RBC_ctrl]),
+                 sort(rownames(out_limma$tests$Chd4BF_vs_RBC_ctrl)[out_limma$tests$Chd4BF_vs_RBC_ctrl$showInVolcano]))
+    expect_equal(abExp$iBAQ.Adnp.avg,
+                 rowMeans(abExp[, c(grep("iBAQ.Adnp_", colnames(abExp)))],
+                          na.rm = TRUE),
+                 ignore_attr = TRUE)
+    expect_equal(abExp$iBAQ.Adnp.sd,
+                 rowSds(abExp[, c(grep("iBAQ.Adnp_", colnames(abExp)))],
+                        na.rm = TRUE),
+                 ignore_attr = TRUE)
+    expect_equal(abExp$LFQ.intensity.Adnp.avg,
+                 rowMeans(abExp[, c(grep("LFQ.intensity.Adnp_", colnames(abExp)))],
+                          na.rm = TRUE),
+                 ignore_attr = TRUE)
+    expect_equal(abExp$LFQ.intensity.Adnp.sd,
+                 rowSds(abExp[, c(grep("LFQ.intensity.Adnp_", colnames(abExp)))],
+                        na.rm = TRUE),
+                 ignore_attr = TRUE)
+
+    ## Two aNames, but only use one
+    abExp <- makeAbundanceExport(testresList = out_limma$tests,
+                                 abundancePrefix = "LFQ.intensity")
+    expect_s3_class(abExp, "data.frame")
+    expect_equal(nrow(abExp), sum(out_limma$tests$RBC_ctrl_vs_Adnp$showInVolcano |
+                                      out_limma$tests$Chd4BF_vs_RBC_ctrl$showInVolcano,
+                                  na.rm = TRUE))
+    expect_equal(ncol(abExp), 18L)
+    expect_equal(abExp[, paste0("LFQ.intensity.", colnames(sce_mq_final))],
+                 as.data.frame(SummarizedExperiment::assay(sce_mq_final,
+                                                           "LFQ.intensity")[abExp$pid, ]),
+                 ignore_attr = TRUE)
+    expect_equal(sort(abExp$pid[abExp$showInVolcano_Chd4BF_vs_RBC_ctrl]),
+                 sort(rownames(out_limma$tests$Chd4BF_vs_RBC_ctrl)[out_limma$tests$Chd4BF_vs_RBC_ctrl$showInVolcano]))
+    expect_equal(abExp$LFQ.intensity.Adnp.avg,
+                 rowMeans(abExp[, c(grep("LFQ.intensity.Adnp_", colnames(abExp)))],
+                          na.rm = TRUE),
+                 ignore_attr = TRUE)
+    expect_equal(abExp$LFQ.intensity.Adnp.sd,
+                 rowSds(abExp[, c(grep("LFQ.intensity.Adnp_", colnames(abExp)))],
                         na.rm = TRUE),
                  ignore_attr = TRUE)
 })
